c6bd96a7e84e0ed832626b8b1f2279c4662726f2
Julija Zitkova <main.zhitkova@gmail.com>
2023-06-11 18:19:41 +0300

    Saku taisit jautajumi un atbildes

--- Žitkova_Eksamens/Assets/Skripti/VardaParadisana.cs
+++ Žitkova_Eksamens/Assets/Skripti/VardaParadisana.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class VardaParadisana : MonoBehaviour {
-	public GameObject ConnectionTesterStatus;
+	
+	public GameObject vards;
+	public GameObject dropDown;
+	public GameObject tests;
 	public string text;
 	public GameObject inputField;
 	public GameObject textAppear;
 	public void saveText(){
 		text = inputField.GetComponent<Text> ().text;
 		textAppear.GetComponent<Text>().text = "Sveiki, " +text.ToUpper()+"!";
+		tests.SetActive (true);
+		vards.SetActive (false);
+	}
+	void Start(){
+		dropDown.SetActive (false);
+		tests.SetActive (false);
 	}
 
 }
