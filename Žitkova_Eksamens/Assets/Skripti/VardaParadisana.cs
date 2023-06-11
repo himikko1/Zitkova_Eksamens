002cb7ba2afe536a082cf9fe95f02c61e359ce61
Julija Zitkova <main.zhitkova@gmail.com>
2023-06-11 17:48:15 +0300

    Saku taisit projektu

---
+++ Žitkova_Eksamens/Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+	public GameObject ConnectionTesterStatus;
+	public string text;
+	public GameObject inputField;
+	public GameObject textAppear;
+	public void saveText(){
+		text = inputField.GetComponent<Text> ().text;
+		textAppear.GetComponent<Text>().text = "Sveiki, " +text.ToUpper()+"!";
+	}
+
+}
